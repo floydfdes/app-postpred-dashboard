2e1a35b2b2f7e75a998da81f5efe03e81eb4ff7b
Floyd Fernandes <floydfernandes20@gmail.com>
2022-09-27 11:55:06 +0530

    Added fonts, colors and primeng.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,21 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'postpred-dashboard';
+  items: any;
+
+  ngOnInit() {
+    this.items = [
+      {
+        label: 'Posts',
+        icon: 'pi pi-fw pi-book',
+      },
+      {
+        label: 'Users',
+        icon: 'pi pi-fw pi-user',
+      },
+    ];
+  }
 }
